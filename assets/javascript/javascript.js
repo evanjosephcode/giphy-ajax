d5f93dfd34f85a6cb3c0dfe9d80207da9876eb52
Evan Seaman <admin@Evans-MacBook-Pro.local>
2018-03-05 11:51:37 -0600

    added a div where img tag will be prepended

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,7 +2,7 @@
 var authKey = "XHQCUcBJCNhE5UjPiKXt5rpOZpSjIDhM";
 
 //hold results from user input
-var searchTerms = "hugh heffner";
+var searchTerms = "disney";
 var numResults = 10;
 var rating = "pg"; 
 
@@ -11,12 +11,25 @@ var topics = ["Jack Nicholson", "Jim Carrey", "Will Ferrell", "Mike Myers", "Mar
 
 var queryURLBase = "https://api.giphy.com/v1/gifs/search?q="+searchTerms+"&api_key="+authKey+"&limit="+numResults+"&rating="+rating
 
+//on click
+$("#gif-button").on("click", function() {
 
-
-$.get(queryURLBase).then(function(GiphyData) {
+$.get(queryURLBase).then(function(response) {
     console.log("URL: "+ queryURLBase);
-    console.log("Giphy data: "+GiphyData);
+
+    var imageURL = response.data.embed_url;
+    //creating and storing image tag
+    var gifImage = $("<img>");
+
+    // setting src to imageURL 
+    gifImage.attr("src", imageURL);
+    gifImage.attr("alt", "gif image");
+
+    //prepending gif image to image div
+    $("#searchPopulated").prepend(gifImage);
+})
 });
 
+// for (var i = 0; i < topics.length, i++) {
 
-
+// }
