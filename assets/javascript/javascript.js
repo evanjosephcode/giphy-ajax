04ee5eea1d5d0756fe969a8a9bb3c763643dcedb
Evan Seaman <admin@Evans-MacBook-Pro.local>
2018-03-05 15:29:33 -0600

    added live and static gif functionality

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,35 +1,95 @@
+$(document).ready(function () {
+
 //api key
 var authKey = "XHQCUcBJCNhE5UjPiKXt5rpOZpSjIDhM";
 
 //hold results from user input
-var searchTerms = "disney";
-var numResults = 10;
+var searchTerms = "oprah";
 var rating = "pg"; 
 
-//array of strings with a for loop we're creating of pre-populated search terms
-var topics = ["Jack Nicholson", "Jim Carrey", "Will Ferrell", "Mike Myers", "Mark Wahlberg"]
+var topics = ["Jack Nicholson", "Jeff Daniels", "Will Ferrell", "Mike Myers", "Mark Wahlberg", "Billy Bob Thornton"]
 
-var queryURLBase = "https://api.giphy.com/v1/gifs/search?q="+searchTerms+"&api_key="+authKey+"&limit="+numResults+"&rating="+rating
+var queryURLBase = "https://api.giphy.com/v1/gifs/search?q="+searchTerms+"&api_key="+authKey+"&limit=10&rating="+rating
 
-//on click
-$("#gif-button").on("click", function() {
 
-$.get(queryURLBase).then(function(response) {
-    console.log("URL: "+ queryURLBase);
+function createButtons() {
+	$("#searchButtons").empty();
 
-    var imageURL = response.data.embed_url;
-    //creating and storing image tag
-    var gifImage = $("<img>");
+	for (var i = 0; i < topics.length; i++) {
 
-    // setting src to imageURL 
-    gifImage.attr("src", imageURL);
-    gifImage.attr("alt", "gif image");
+		var make = $("<button>");
+		make.addClass("topicButton").attr("data-name", topics[i]).text(topics[i]);
+		$("#searchButtons").append(make);
+	}
+}
 
-    //prepending gif image to image div
-    $("#searchPopulated").prepend(gifImage);
-})
+// perform the function that contains the for loop just created 
+createButtons();
+
+// check for existing val() in array called topics, and push if not in there there and re-perform createButtons() to update buttons
+$("#addSearch").on("click", function () {
+	event.preventDefault();
+	var userInput = $("#searchInput").val().trim();
+
+	if (topics.indexOf(userInput) !== -1) {
+		alert("that's already a button, fool");
+	}
+
+	else {
+		topics.push(userInput);
+		createButtons();
+	}
+
+
 });
 
-// for (var i = 0; i < topics.length, i++) {
+$(document).on("click", ".topicButton", function(response) {
+	var dataNames = $(this).attr("data-name");
+	var queryURLBase = "https://api.giphy.com/v1/gifs/search?q="+dataNames+"&api_key="+authKey+"&limit=10&rating="+rating;
 
-// }
+	$.get(queryURLBase).then(function(response) {
+
+	$("#searchPopulated").empty();
+	
+	for (var i = 0; i < response.data.length; i++) {
+
+		var imageURL = response.data[i].images.fixed_height_still.url;
+		var movingURL = response.data[i].images.fixed_width.url;
+
+		//creating and storing image tag
+		var gifImage = $("<img>");
+
+		// setting src to imageURL 
+		gifImage.attr("src", imageURL);
+		gifImage.attr("data-still", imageURL);
+		gifImage.attr("data-animate", movingURL);
+		gifImage.attr("data-state", "still");
+		gifImage.attr("alt", "gif image");
+		gifImage.addClass("gifClick");
+	
+		//prepending gif image to image div
+		$("#searchPopulated").prepend(gifImage);
+	}
+	});
+});
+
+
+$(document).on("click", ".gifClick", function(response) {
+	var state = $(this).attr("data-state");
+
+	if (state === "still") {
+		$(this).attr("src", $(this).attr("data-animate"));
+		$(this).attr("data-state", "animate");
+	}
+	else {
+		$(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+	}
+});
+
+
+
+
+
+// to end the document ready
+});
